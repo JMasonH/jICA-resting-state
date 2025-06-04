2ad94aa7065081b1eb91acb493f75b4dadf2c5fa
J. Mason Harding <145717369+JMasonH@users.noreply.github.com>
2025-06-04 13:51:20 -0500

    Update spatial_ICA.sh

--- prepare_data/spatial_ICA.sh
+++ prepare_data/spatial_ICA.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Check that both arguments were provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <input_txt_path> <output_dir>"
+  exit 1
+fi
+
+input_file=$1
+output_dir=$2
+
+# Load required modules
 module load GCC/5.4.0-2.26 
 module load OpenMPI/1.10.3
 module load FSL/5.0.10
 
-melodic -i /fs1/neurdylab/projects/jICA/test_pipe/fmri_test_paths.txt -d 40 -o /fs1/neurdylab/projects/jICA/test_pipe --Oorig --report --tr=2.1 -v
+# Run MELODIC
+melodic -i "$input_file" -d 40 -o "$output_dir" --Oorig --report --tr=2.1 -v
 
 
 
