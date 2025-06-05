5115359b1e86e44b0104b0d16bef883ecca11b58
J. Mason Harding <145717369+JMasonH@users.noreply.github.com>
2025-06-05 16:01:47 -0500

    Update subject_jICA_maps.m

--- single_subject_jICA/subject_jICA_maps.m
+++ single_subject_jICA/subject_jICA_maps.m
@@ -1,14 +1,12 @@
+function subject_jICA_maps(workDir, maskPath) 
 
-function subject_jICA_maps(data_dir) 
+sub_mat = dir(fullfile(workDir, 'single_subject_jICA_projections', '*.mat'));
 
-sub_mat = dir(fullfile(data_dir, '*.mat'));
-
-addpath('/fs1/neurdylab/projects/jICA/');
-addpath('/fs1/neurdylab/software/eeglab2021')
-blank_scalp = load("/fs1/neurdylab/datasets/eegfmri_vu/PROC/vpat15/eeg/scan02/vpat15-scan02_eeg_pp.mat");
+addpath(eeglab_path) %add eeglab path
+blank_scalp = load(eeg_dummy_path);
 eeglab;
 
-maskfile = "fs1/neurdylab/projects/jICA/MNI152_T1_2mm_brain_mask_filled.nii.gz"; %control ic maps
+maskfile = maskPath; %control ic maps
 
 
 brain_mask = niftiread(maskfile);
@@ -17,7 +15,7 @@ mask_inds = find(brain_mask ~= 0);
 
 for i = 1:length(sub_mat)
 
-    mat = load(fullfile(data_dir, sub_mat(i).name));
+    mat = load(fullfile(workDir, 'single_subject_jICA_projections', sub_mat(i).name));
 
     
     n = 13;  % Specify the number of characters to keep
@@ -30,7 +28,7 @@ for i = 1:length(sub_mat)
     id = fileName(1:6);
     scan = fileName(8:13);
 
-    folderName = ['/fs1/neurdylab/projects/jICA/test_pipe/jICA-neuroimaging-epilepsy/single_subject_jICA_projections/spatial_maps/', id, '-', scan];  % Specify the folder name
+    folderName = [fullfile(workDir, 'single_subject_jICA_projections', 'spatial_maps'), '/', id, '-', scan];  % Specify the folder name
 
     if ~exist(folderName, 'dir')
         mkdir(folderName);
@@ -43,7 +41,7 @@ for i = 1:length(sub_mat)
 
     %/fs1/neurdylab/datasets/eegfmri_vu/PROC/vcon12/meica_proc_scan01/meica_out/vcon12-scan01_EPI2MNI_sm.nii.gz
 
-    fmri = load(['/fs1/neurdylab/projects/jICA/test_pipe/jICA-neuroimaging-epilepsy/fmri_projections/', id, '-', scan, '_IC_reg.mat']);
+    fmri = load([fullfile(workDir, 'fmri_projections'), '/', id, '-', scan, '_IC_reg.mat']);
     spatmap = fmri.OUT.spatial;
     sub_spatmap = spatmap(2:end, :);
 
